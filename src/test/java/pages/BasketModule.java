c211efe4d172eacb123166168ffe6f100759d6a8
Nikita Sukhodoev <dogmach94@yandex.ru>
2022-07-19 13:49:01 +0400

    Сделал проект по шаблону проектирования "PageObject"

--- src/test/java/pages/BasketModule.java
+++ src/test/java/pages/BasketModule.java
@@ -9,16 +9,14 @@ import static com.codeborne.selenide.Selenide.$x;
 public class BasketModule {
     private SelenideElement burgerLine = $(".nav-element__burger-line"),
             menuBurger = $x("//a[@class='menu-burger__main-list-link menu-burger__main-list-link--566']"),
-            plants = $x("//div[contains(@class,'menu-burger__drop-list-item j-menu-drop-item j-menu-drop-item-566" +
-                    " menu-burger__drop-list-item--active')]//a[contains(@class,'menu-burger__link" +
-                    " j-menu-drop-link')][contains(text(),'Брюки')]"),
+            plants = $("body > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > div:nth-child(2) > div:nth-child(12) > div:nth-child(4) > div:nth-child(1) > ul:nth-child(8) > li:nth-child(1) > a:nth-child(2)"),
             plantsOfInterest = $x("//img[@alt='Брюки STR style']"),
             size48 = $x("//span[normalize-space()='48']"),
             buttonBasket = $x("//div[contains(@class,'product-page__aside-container j-price-block')]//span[contains(@class,'hide-mobile')][contains(text(),'Добавить в корзину')]"),
             goToBasket = $x("//div[contains(@class,'product-page__aside-container j-price-block')]//a[contains(@class,'btn-base j-go-to-basket')][contains(text(),'Перейти в корзину')]"),
             goodInfoGoodName = $x("//span[@class='good-info__good-brand ']");
 
-    private String platsFullName = "Брюки, STR style";
+    private String platsFullName = "Брюки";
 
     public BasketModule clickToBurgerLine() {
         burgerLine.click();
@@ -56,7 +54,7 @@ public class BasketModule {
     }
 
     public BasketModule checkAvailabilityPlants() {
-        goodInfoGoodName.shouldHave(Condition.text(platsFullName));
+        goodInfoGoodName.shouldHave(Condition.attribute("Штаны"))
         return this;
     }
 
